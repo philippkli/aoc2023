b4e51c26d03b820cb0f18758b86a3bccdc8d25c7
philippkli <philipp.klingert@volkswagen-groupservices.com>
2023-12-14 10:07:58 +0100

    working solution
    its still ugly but it has to suffice

--- day1/day1part2.dart
+++ day1/day1part2.dart
@@ -2,66 +2,104 @@ import 'dart:io';
 
 void main() {
   readFile();
+  List<dynamic> endLineList = [];
+  num sumOfLists = 0;
   for (var l in readFile()) {
     List<String> singleline = l.split(' ');
-    print("Line: $l");
-    print("Line length: ${singleline.length}");
-    print("------------");
+    List<dynamic> collection = [];
+    String element = singleline[0];
 
-    for (int i = 0; i < singleline.length; i++) {
-      String element = singleline[i];
-      for (int j = 0; j < element.length; j++) {
-        if (element[j] == 'o' && j+2 < element.length && element[j+1] == 'n' && element[j+2] == 'e') {
-          print('1');
-        } else if (element[j] == 't' && j+2 < element.length && element[j+1] == 'w' && element[j+2] == 'o') {
-          print('2');
-        } else if (element[j] == 't' && j+4 < element.length && element[j+1] == 'h' && element[j+2] == 'r' && element[j+3] == 'e' && element[j+4] == 'e') {
-          print('3');
-        } else if (element[j] == 'f' && j+3 < element.length && element[j+1] == 'o' && element[j+2] == 'u' && element[j+3] == 'r') {
-          print('4');
-        } else if (element[j] == 'f' && j+3 < element.length && element[j+1] == 'i' && element[j+2] == 'v' && element[j+3] == 'e') {
-          print('5');
-        } else if (element[j] == 's' && j+2 < element.length && element[j+1] == 'i' && element[j+2] == 'x') {
-          print('6');
-        } else if (element[j] == 's' && j+4 < element.length && element[j+1] == 'e' && element[j+2] == 'v' && element[j+3] == 'e' && element[j+4] == 'n') {
-          print('7');
-        } else if (element[j] == 'e' && j+4 < element.length && element[j+1] == 'i' && element[j+2] == 'g' && element[j+3] == 'h' && element[j+4] == 't') {
-          print('8');
-        } else if (element[j] == 'n' && j+3 < element.length && element[j+1] == 'i' && element[j+2] == 'n' && element[j+3] == 'e') {
-          print('9');
-        }
+    print("------------");
+    print("Line: $l");
+
+    for (int j = 0; j < element.length; j++) {
+      if (element[j] == 'o' &&
+          j + 2 < element.length &&
+          element[j + 1] == 'n' &&
+          element[j + 2] == 'e') {
+        collection.add(1);
+      } else if (element[j] == 't' &&
+          j + 2 < element.length &&
+          element[j + 1] == 'w' &&
+          element[j + 2] == 'o') {
+        collection.add(2);
+      } else if (element[j] == 't' &&
+          j + 4 < element.length &&
+          element[j + 1] == 'h' &&
+          element[j + 2] == 'r' &&
+          element[j + 3] == 'e' &&
+          element[j + 4] == 'e') {
+        collection.add(3);
+      } else if (element[j] == 'f' &&
+          j + 3 < element.length &&
+          element[j + 1] == 'o' &&
+          element[j + 2] == 'u' &&
+          element[j + 3] == 'r') {
+        collection.add(4);
+      } else if (element[j] == 'f' &&
+          j + 3 < element.length &&
+          element[j + 1] == 'i' &&
+          element[j + 2] == 'v' &&
+          element[j + 3] == 'e') {
+        collection.add(5);
+      } else if (element[j] == 's' &&
+          j + 2 < element.length &&
+          element[j + 1] == 'i' &&
+          element[j + 2] == 'x') {
+        collection.add(6);
+      } else if (element[j] == 's' &&
+          j + 4 < element.length &&
+          element[j + 1] == 'e' &&
+          element[j + 2] == 'v' &&
+          element[j + 3] == 'e' &&
+          element[j + 4] == 'n') {
+        collection.add(7);
+      } else if (element[j] == 'e' &&
+          j + 4 < element.length &&
+          element[j + 1] == 'i' &&
+          element[j + 2] == 'g' &&
+          element[j + 3] == 'h' &&
+          element[j + 4] == 't') {
+        collection.add(8);
+      } else if (element[j] == 'n' &&
+          j + 3 < element.length &&
+          element[j + 1] == 'i' &&
+          element[j + 2] == 'n' &&
+          element[j + 3] == 'e') {
+        collection.add(9);
+      } else if (element[j] == '1') {
+        collection.add(1);
+      } else if (element[j] == '2') {
+        collection.add(2);
+      } else if (element[j] == '3') {
+        collection.add(3);
+      } else if (element[j] == '4') {
+        collection.add(4);
+      } else if (element[j] == '5') {
+        collection.add(5);
+      } else if (element[j] == '6') {
+        collection.add(6);
+      } else if (element[j] == '7') {
+        collection.add(7);
+      } else if (element[j] == '8') {
+        collection.add(8);
+      } else if (element[j] == '9') {
+        collection.add(9);
       }
     }
+    int combinedValue = int.parse('${collection[0]}${collection[collection.length-1]}');
+    endLineList = [combinedValue];
+    print(endLineList);
+
+    sumOfLists += endLineList[0];
+    print("Adding ${endLineList[0]}");
+    print("Current Sum: $sumOfLists");
   }
+  print("Total Sum: ${sumOfLists}");
 }
 
 List<dynamic> readFile() {
-  final inputdayone = File("testinputpart2.txt");
+  final inputdayone = File("input.txt");
   List<dynamic> lines = inputdayone.readAsLinesSync();
   return lines;
-}
-
-// use this
-void func() {
-  String test = 'Hallo';
-  print(test.substring(1));
-  print(test.startsWith('Ha'));
-  print(test.endsWith('llo'));
-}
-
-// theorie 1: Array abgleichen da eintrag two, im array die index 2 hat also two = zwei, und dann den integer wiedergeben
-int convertStringToInt(String str) {
-  var words = [
-    'zero',
-    'one',
-    'two',
-    'three', 
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-  ];
-  return words.indexOf(str);
 }
